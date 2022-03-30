74467c9064567b13ecd855df13c69b89a4a86c37
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2022-03-30 18:57:55 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-// import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      {/* <Head>
-        <script
-          src="https://kit.fontawesome.com/81c88ca9af.js"
-          crossOrigin="anonymous"
-        ></script>
+      
       <Component {...pageProps} />
     </div>
   )
