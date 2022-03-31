089ed601e82afdde722e775afa48cc7a2e2831d2
Gopa Venkatesh <gopavenkatesh@AGLS-MLT-473.local>
2022-03-31 09:31:41 +0530

    icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      
+      <Script
+        src="https://kit.fontawesome.com/81c88ca9af.js"
+        crossOrigin="anonymous"
+      ></Script>
       <Component {...pageProps} />
     </div>
   )
